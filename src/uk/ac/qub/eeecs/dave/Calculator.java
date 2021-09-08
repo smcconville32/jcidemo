10bcf866885a32486d831fb4810654a838df615e
smcconville32 <smcconville32@qub.ac.uk>
2021-09-08 10:58:01 +0100

    breaking tests

--- src/uk/ac/qub/eeecs/dave/Calculator.java
+++ src/uk/ac/qub/eeecs/dave/Calculator.java
@@ -2,14 +2,14 @@ package uk.ac.qub.eeecs.dave;
 
 public class Calculator {
 	public int add(int a, int b) {
-		return a+b;
+		return (a+b)+1;
 	}
 	
 	public int sub(int a, int b) {
-		return a-b;
+		return (a-b)+1;
 	}
 	public double doubleadd(double a, double b) {
-		return a+b;
+		return (a+b)+1;
 	}
 	
 }
